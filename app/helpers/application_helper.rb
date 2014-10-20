ee4fa256b240498cb10f328a3a1ab7873f41cfff
sawan <sawan.gupta92@gmail.com>
2014-10-20 12:34:55 +0530

    show header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
     YAML.load_file('config/config.yml')['company']['domain']
   end
 
+  def admin_logged_in
+    current_user.admin
+  end
+
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,7 +3,7 @@ class Setting < ActiveRecord::Base
   before_save :uploaded_path_url?
 
   def uploaded_path_url?
-    if value.split('.').last == 'png'
+    if value.split('.').last == 'jpg'
       true
     else
       errors.add(:value, "not valid only .png file will be accepted")
