dcbf3c34b159c345231866135db8773ace589f16
sawan <sawan.gupta92@gmail.com>
2014-10-24 23:12:39 +0530

    edit settings page for multiple users

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,50 +1,35 @@
 class SettingsController < ApplicationController
 
-  # fix- Rename. Use some meaningful name.
+  # fix- Rename. Use some meaningful name. -DONE
   before_action :authenticate_admin
 
-  # def new
-    # @settings = Setting.new
-  # end
+  # fix- This may not be required. Lets discuss. -DONE
 
-  # fix- This may not be required. Lets discuss.
-  def create
+  def edit
 
-    params[:key].size.times do |index|
-      setting = Setting.find_by(key: params[:key][index][index.to_s])
-      if setting
-        setting.update_column(:value, params[:value][index][index.to_s])
-      else
-        Setting.create(key: params[:key][index][index.to_s], value: params[:value][index][index.to_s])
-      end
-    end
+    @first_setting = Setting.find_or_create_by(key: :logo)
+    @second_setting = Setting.find_or_create_by(key: :location)
 
-    redirect_to :root
+    # fix- This will not work. As there may be more than 1 keys i.e. Setting objects. A single Setting object stores 1 key/value pair -DONE
   end
 
   def update
+    params[:settings].each do |setting|
+      @setting = Setting.find(setting.first)
+      @setting.update(setting.second)
+    end
+    redirect_to :root
     # Move following line of code to before_action. Redirect to root page if no setting object exists.
-    # setting = Setting.first
     # fix- Below code wont work if there are more than one keys need to be updated.
-    #       Discuss with me if unclear.
-    # unless setting.update_attribute(:value, allowed_params[:value])
-      # fix- Don't use redirect to back. Redirect to root page instead with a message
+    #       Discuss with me if unclear. -DONE
+      # fix- Don't use redirect to back. Redirect to root page instead with a message -DONE
       # fix- Handle failure case also.
-      # redirect_to :back
-    # end
   end
 
-  def edit
-    # fix- This will not work. As there may be more than 1 keys i.e. Setting objects. A single Setting object stores 1 key/value pair
-    # @setting = Setting.find_or_create_by(key: :logo)
-  end
+  private
 
-  def allowed_params
-    # params.require(:setting).permit(:value, :key)
-  end
-
-  def authenticate_admin
-    redirect_to :root unless current_user.admin
-  end
+    def authenticate_admin
+      redirect_to :root unless current_user.admin
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { confirmations: 'users/confirmations', passwords: 'users/passwords' }
   root 'users#index'
-  resources :settings, only: [:new, :create, :update, :edit]
+  resources :settings, only: [:update, :edit]
 
 end
