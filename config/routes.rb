58f0e1d0d76f3ad913d039a2837fed14e51aa53c
sawan <sawan.gupta92@gmail.com>
2014-10-26 22:18:25 +0530

    edit user details

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { confirmations: 'users/confirmations' }
+  devise_for :users, controllers: { confirmations: 'users/confirmations', sessions: 'users/sessions' }
   root 'application#index'
   resources :settings, only: [:new, :create, :update]
   resources :users, only: [:index, :edit, :update]
