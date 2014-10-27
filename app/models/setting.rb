83b882bb480745c0f30806e62f650dc596e9b219
sawan <sawan.gupta92@gmail.com>
2014-10-27 16:33:03 +0530

    fix issue

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,13 +3,6 @@ class Setting < ActiveRecord::Base
   before_save :uploaded_path_url?
 
   #FIX: Check if this method has been removed on 'develop'
-  def uploaded_path_url?
-    if value.split('.').last == 'jpg'
-      true
-    else
-      errors.add(:value, "not valid only .png file will be accepted")
-      false
-    end
-  end
+  # yes it was removed in develop
 
 end
