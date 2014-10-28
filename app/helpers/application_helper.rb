843bed0ad969472bc62bde7941e128534a5cec56
sawan <sawan.gupta92@gmail.com>
2014-10-28 15:41:14 +0530

    fix sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 
-  #FIX: Rename. Use just #company_config.
   def company_config
     YAML.load_file('config/config.yml')['company']
   end
@@ -9,23 +8,18 @@ module ApplicationHelper
     current_user.admin
   end
 
-  #FIX: When user visits the index page for first time, each field should have links to sort in asc/desc order.
-  #FIX: When the list is sorted on a field, that field should have the link to order in opposite direction and other fields should have both links.
-  #FIX: Use something else instead of variable names 'link' and 'order'. Something more clear.
-
-  def generate_image_tag(order, direction)
+  def sort_direction_link(order, direction)
     image = image_tag("#{direction}.png")
     link_to image, users_path(order: order, direction: direction)
   end
 
-  def link_to_by_order(link, order_by)
-    sort_order = params[:direction] == "asc" ? :desc : :asc
-    if link.downcase == params[:order]
-      generate_image_tag params[:order], sort_order
+  def link_to_by_order(link)
+    sort_order = params[:direction] == 'asc' ? :desc : :asc
+    if link.to_s == params[:order]
+      sort_direction_link params[:order], sort_order
     else
-      # order = params[:order] == 'name' ? 'email' : 'name'
-      sort_by_ascending_image = generate_image_tag order_by, :asc
-      sort_by_descending_image = generate_image_tag order_by, :desc
+      sort_by_ascending_image = sort_direction_link link, :asc
+      sort_by_descending_image = sort_direction_link link, :desc
       [sort_by_ascending_image, sort_by_descending_image].join().html_safe
     end
   end
@@ -33,4 +27,5 @@ module ApplicationHelper
   def admin_or_self_user(user)
     admin_logged_in or user == current_user
   end
+
 end
