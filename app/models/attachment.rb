6a85acee0a6eff7bf04d1da3547b6844ae4729b0
sawan <sawan.gupta92@gmail.com>
2014-10-31 01:00:42 +0530

    add options

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,7 +2,7 @@ class Attachment < ActiveRecord::Base
 
   belongs_to :attachment, polymorphic: true
 
-  has_attached_file :attachment, :styles => { :original => '100x100>' }
+  has_attached_file :attachment, :styles => { :original => '100x100>' }, url: '/:class/:id/:style.:extension', path: ':rails_root/public:url'
 
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 
