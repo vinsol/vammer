38792fbb96bc46fe30bc34b6936e7347791d6fb9
sawan <sawan.gupta92@gmail.com>
2014-11-01 13:39:03 +0530

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :fetch_logo
+  before_action :authenticate_user!, :fetch_logo, :fetch_groups
 
   def after_sign_in_path_for(resource)
     root_path
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   def fetch_logo
     @setting = Setting.where(key: :logo).first
+  end
+
+  def fetch_groups
+    @my_groups = current_user.groups
   end
 
 end
