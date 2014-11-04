b4c00e8089c1a530a151701bdd7b7292e464e123
Akhil Bansal <bansalakhil30.10@gmail.com>
2014-11-04 21:24:46 +0530

    Some CR comments added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
+#FIXME_AB: Indexes missing on almost all tables
 class User < ActiveRecord::Base
   
   include Configuration
 
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable, :confirmable
-
+  #FIXME_AB: group all associations, validations and callbacks together. Do it in all models
+  #FIXME_AB: No association should be defined without dependent option, in any model
   has_one :image, as: :attachment
 
   accepts_nested_attributes_for :image
@@ -42,6 +44,7 @@ class User < ActiveRecord::Base
     end
 
     def set_enabled
+      #FIXME_AB: This should be the default column value, so that we don't need to set it when creating.
       self.enabled = true
     end
 
