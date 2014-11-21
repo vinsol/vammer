bbe99d3df3c1f86cf7a36cddce2472929337f7cc
sawan <sawan.gupta92@gmail.com>
2014-11-21 16:32:32 +0530

    modal for ajax is left

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,7 @@ class Document < Attachment
   has_attached_file :attachment
 
   VALID_FILE_TYPE = %w(application/pdf application/msword 
-    application/msexcel application/mspowerpoint)
+    application/vnd.ms-excel application/mspowerpoint)
   validates_attachment_content_type :attachment, content_type: [/\Aimage\/.*\Z/, VALID_FILE_TYPE]
 
 end
